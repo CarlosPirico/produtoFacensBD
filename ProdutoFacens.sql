930b1bc2bb5207aa816a243f7f1df14d7d758f4c
Carlos Pirico <carlos.pirico@hotmai.com>
2024-10-23 18:25:03 -0300

    att bd

--- ProdutoFacens.sql
+++ ProdutoFacens.sql
@@ -21,30 +21,3 @@ INSERT INTO `produto` (`id_produto`, `nome`, `preco`, `estoque`, `perecivel`, `m
 	(3, 'Chocolate', 9.99, 50, 'Não', 'Cadbury', 'Britânica '),
 	(4, 'Salgadinho', 3.59, 66, 'Sim', 'Lays', 'Norte-americano'),
 	(5, 'Arroz', 5.55, 76, 'Sim', 'Tio João', 'Brasileiro');
-
-/* SELECTS */;
-/* 
-// Quantos produtos estão cadastrados
-SELECT COUNT(*) AS qtd_produto FROM produto;
-
-// Preço médio dos produtos
-SELECT SUM(p.preco)/COUNT(p.preco) AS preco_medio_produto FROM produto p;
-
-// média dos preços dos produtos em 2 grupos: perecíveis e não perecíveis;
-SELECT p.perecivel, SUM(p.preco)/COUNT(p.preco) AS preco_medio_produto FROM produto p GROUP BY p.perecivel;
-
-// média dos preços dos produtos agrupados pelo nome do produto
-SELECT p.nome, SUM(p.preco)/COUNT(p.preco) AS preco_medio_produto FROM produto p GROUP BY p.nome;
-
-// média dos preços e total em estoque dos produtos
-SELECT SUM(p.preco)/COUNT(p.preco) AS preco_medio_produto, SUM(p.estoque) AS estoque FROM produto p;
-
-// nome, marca e quantidade em estoque do produto mais caro
-SELECT p.nome, p.marca, p.estoque FROM produto p ORDER BY p.preco DESC;
-
-// produtos com preço acima da média
-SELECT p.* FROM produto p WHERE p.preco > (SELECT SUM(p.preco)/COUNT(p.preco) AS preco_medio_produto FROM produto p);
-
-// quantidade de produtos de cada nacionalidade
-SELECT p.nacionalidade, COUNT(p.nacionalidade) AS estoque FROM produto p GROUP BY p.nacionalidade; 
-*/;
